b2ee89a827972063c6391014b5861e123fd6a9cb
FredX <frederic@e-founders.com>
2017-10-03 19:20:05 +0200

    catch transfer in webhooks

--- backend/routes/treezor/webHookHandler.js
+++ backend/routes/treezor/webHookHandler.js
@@ -3,44 +3,45 @@ const InvoiceService = require('../../services/invoice');
 
 module.exports = async (req, res) => {
   //Take first transaction for the POC
-  const transac = req.body.object ? req.body.object === 'transaction' : false
-  console.log("Body: -------------------- " + req.body.object)
-  if( transac ) {
-    const transacPayload = req.body["object_pay load"].transactions[0];
-    const description = transacPayload.description ? transacPayload.description : 'Unknown';
-    const amount = transacPayload.amount ? transacPayload.amount : "O";
+  const transfer = req.body.object ? req.body.object === 'transfer' : false
+  console.log(req.body)
+  console.log(req.body.object_payload)
 
-    try {
-      const Organization = await OrganizationService.findByShortId('eclub');
-      const payment = await Organization.createPayment({
-        amount: {
-          amount: amount,
-          currency: 'eur'
-        },
-        origin: 'aircall.io',
-        description: description,
-        status: 'unmatched'
-      })
+    const transferPayload = req.body.object_payload.transfers[0];
+    if( transferPayload.transferStatus ===  'VALIDATED') {
+      const description = transferPayload.label ? transferPayload.label : 'Unknown';
+      const amount = transferPayload.amount ? transferPayload.amount : "O";
 
-      const parsedInvoiceId = description.split('#')[1];
-      console.log("parsed-----------: " + parsedInvoiceId);
-      if (parsedInvoiceId) {
-        const invoice = await InvoiceService.findByCustomId(parsedInvoiceId);
-        if(invoice){
-          await invoice.update({
-            status: 'paid'
-          }).then(() => {})
-          await payment.update({
-              status: 'matched'
+      try {
+        const Organization = await OrganizationService.findByShortId('eclub');
+        const payment = await Organization.createPayment({
+          amount: {
+            amount: amount,
+            currency: 'eur'
+          },
+          origin: 'aircall.io',
+          description: description,
+          status: 'unmatched'
+        });
+
+        const parsedInvoiceId = description.split('#')[1];
+        console.log("parsed-----------: " + parsedInvoiceId);
+        if (parsedInvoiceId) {
+          const invoice = await InvoiceService.findByCustomId(parsedInvoiceId);
+          if(invoice){
+            await invoice.update({
+              status: 'paid'
             }).then(() => {})
+            await payment.update({
+                status: 'matched'
+              }).then(() => {})
+          }
         }
-
+      } catch (error) {
+          console.error('Create Payment from Treezor WebHook:', error);
+          return res.json({ error: 'ERROR' });
       }
-    } catch (error) {
-        console.error('Create Payment from Treezor WebHook:', error);
-        return res.json({ error: 'ERROR' });
     }
-  }
 
   res.json({
     ts: Date.now(),
